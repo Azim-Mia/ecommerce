cd512942832ab7a4894d1d1bfada27209e0161b4
azim <mdazimmia53@gmail.com>
2026-05-06 20:25:46 +0600

    change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/ecommerce",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
