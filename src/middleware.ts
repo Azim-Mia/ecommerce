7a19cab4569d9ea315cc203e7e80442356611cad
azim <mdazimmia53@gmail.com>
2026-05-06 15:12:17 +0600

    change

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-// src/middleware.ts
-
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export function middleware(request: NextRequest) {
-  return NextResponse.next();
-}
